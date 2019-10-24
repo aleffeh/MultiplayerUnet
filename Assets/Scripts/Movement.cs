7896c1ce4c9e90435c2c935295f41cc8c4283133
Felipe Oliveira <felipexk1@live.com>
2019-10-24 14:46:06 -0300

    Movement done

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : NetworkBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    private Rigidbody2D _rb2d;
+
+    private void Awake() => _rb2d = GetComponent<Rigidbody2D>();
+    private void FixedUpdate() => Move(GetDirection());
+    private Vector2 GetDirection() =>
+        new Vector2 {x = Input.GetAxisRaw("Horizontal"), y = Input.GetAxisRaw("Vertical")};
+    private void Move(Vector2 direction)
+    {
+        if (isLocalPlayer)
+            _rb2d.velocity = direction * speed;
+    }
+}
